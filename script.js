d46da9dd5a75b86fd36f20227601eb37d32ba658
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-21 19:56:50 +0300

    optimized code

--- script.js
+++ script.js
@@ -20,18 +20,21 @@ let data = {
 
 function TreeFromObject(obj) {
   const ul = document.createElement('ul');
+
   for (const key in obj) {
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     li.textContent = key;
+
     if (typeof obj[key] === 'object' && Object.keys(obj[key]).length > 0) {
       li.appendChild(TreeFromObject(obj[key]));
-      console.log(li);
     }
+
     ul.appendChild(li);
   }
+
   return ul;  
 }
 
-document.body.appendChild(TreeFromObject(data));
+document.querySelector('#treeFromObj').appendChild(TreeFromObject(data));
 
 
